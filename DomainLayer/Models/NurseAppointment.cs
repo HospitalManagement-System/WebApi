ff92675a1635e26654e867c225764de41ba80d2c
Vamsikrishna Boligirla <Vamsikrishna.Boligirla@citiustech.com>
2022-01-31 13:25:44 +0530

    Webapicomiit

--- DomainLayer/Models/NurseAppointment.cs
+++ DomainLayer/Models/NurseAppointment.cs
@@ -9,14 +9,20 @@ namespace DomainLayer.Models
     public class NurseAppointment
     {
         public Guid Id { get; set; }
-        public string FirstName { get; set; }   
-        public char Gender { get; set; }
+        public string Name { get; set; }   
+        public string Gender { get; set; }
         public string Diagnosis { get; set; }
-        public int Contact { get; set; }
+        public string Contact { get; set; }
         public int Age { get; set; }
         public string Email { get; set; }
         public string PhysicanName { get; set; }
         public string Date { get; set; }
-       
+        public string BookSlot { get; set; }
+
+        public DateTime AppointmentDateTime { get; set; }
+
+
+
+
     }
 }
